171faf7ba557ca35b935ae625ad80efd7b67e5a6
aaaeide <ae@pilco.no>
2021-12-01 11:16:15 +0100

    Day 01 refactor

--- lib/day01.ex
+++ lib/day01.ex
@@ -2,20 +2,18 @@ defmodule Aoc21.Day01 do
   import Aoc21.Input, only: [readlines: 2]
 
   def count_depth_measurement_increase(measurements) do
-    measurements
-    |> Enum.reduce({0, :infinity}, fn depth, {count, prev} ->
-      if depth > prev,
-        do: {count + 1, depth},
-        else: {count, depth}
-    end)
-    |> elem(0)
+    find_increase(measurements) |> Enum.sum()
   end
 
-  def construct_sliding_window_sums([a, b, c | []]), do: [a + b + c]
+  def find_increase([a, b | rest]) when a < b, do: [1 | find_increase([b | rest])]
+  def find_increase([a, b | rest]) when a >= b, do: [0 | find_increase([b | rest])]
+  def find_increase(_), do: []
 
   def construct_sliding_window_sums([a, b, c | rest]) do
     [a + b + c | construct_sliding_window_sums([b, c | rest])]
   end
+
+  def construct_sliding_window_sums(_), do: []
 
   def part1() do
     readlines("i1.txt", as: :integer)
