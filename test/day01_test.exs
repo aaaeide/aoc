fb5b1e314d3e85c77107248673a5201ad5782f46
aaaeide <ae@pilco.no>
2021-12-01 13:37:56 +0100

    Day 01 refactor

--- test/day01_test.exs
+++ test/day01_test.exs
@@ -1,6 +1,5 @@
 defmodule Day01Test do
   use ExUnit.Case
-  doctest Aoc21.Day01
 
   import Aoc21.Day01,
     only: [
@@ -8,44 +7,21 @@ defmodule Day01Test do
       construct_sliding_window_sums: 1
     ]
 
-  @depths "199
-200
-208
-210
-200
-207
-240
-269
-260
-263
-"
-
-  @sliding_window_sums "607
-618
-618
-617
-647
-716
-769
-792
-"
-
-  def read_integers(string) do
-    string
-    |> String.split("\n", trim: true)
-    |> Enum.map(&String.to_integer(&1))
+  setup_all do
+    [
+      depths: [199, 200, 208, 210, 200, 207, 240, 269, 260, 263],
+      sliding_window_sums: [607, 618, 618, 617, 647, 716, 769, 792]
+    ]
   end
 
-  test "count increases works on test input" do
-    assert @depths
-           |> read_integers()
+  test "count increases works on test input", fixture do
+    assert fixture.depths
            |> count_depth_measurement_increase() == 7
   end
 
-  test "constructs sliding window sums correctly on test input" do
-    assert @depths
-           |> read_integers()
+  test "constructs sliding window sums correctly on test input", fixture do
+    assert fixture.depths
            |> construct_sliding_window_sums() ==
-             read_integers(@sliding_window_sums)
+             fixture.sliding_window_sums
   end
 end
