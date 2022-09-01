a2724d9c8a156ff3ae31c8b40e8cfdf1e8565f36
Andreas Aaberge Eide <ae@pilco.no>
2022-09-01 12:57:27 +0200

    Attempt Day 11

--- lib/day11.ex
+++ lib/day11.ex
@@ -1,17 +1,71 @@
 defmodule Aoc21.Day11 do
-  @spec step([[integer()]]) :: [[integer()]]
-  def step(grid) do
-    grid
+  @type grid :: [[integer()]]
+  @type coord :: {integer(), integer()}
+  @type coordset :: MapSet.t({integer(), integer()})
 
+  @spec step(grid(), integer(), integer()) :: {grid(), integer()}
+  def step(grid, n, cnt \\ 0)
+
+  def step(grid, 0, flash_cnt), do: {grid, flash_cnt}
+
+  def step(grid, n, flash_cnt) do
+    {new_grid, num_new_flashes} =
+      grid
+      |> increment_cells
+      |> cascade(MapSet.new(), MapSet.new())
+
+    step(new_grid, n - 1, flash_cnt + num_new_flashes)
   end
 
-  @spec increment_cells([[integer()]], MapSet) :: list
+  @spec cascade(grid(), coordset(), coordset(), integer() | nil) :: {grid(), integer()}
+  def cascade(grid, all_flashed, new_flashed, num_new_flashed \\ nil)
+
+  def cascade(grid, all_flashed, _new_flashed, 0),
+    do: {grid, MapSet.size(all_flashed)}
+
+  def cascade(grid, all_flashed, new_flashed, _num_new_flashed) do
+    # find all cells that need to be incremented and by how much
+    to_inc =
+      new_flashed
+      |> Enum.map(&get_neighbs/1)
+      |> Enum.reduce(%{}, fn coordset, map ->
+        coordset
+        |> Enum.reduce(map, fn coord, map ->
+          Map.update(map, coord, 1, fn cur -> cur + 1 end)
+        end)
+      end)
+
+    # increment cells
+    new_grid =
+      grid
+      |> Enum.with_index()
+      |> Enum.map(fn {row, y} ->
+        row
+        |> Enum.with_index()
+        |> Enum.map(fn {val, x} ->
+          if Map.has_key?(to_inc, {x, y}) do
+            val + to_inc[{x, y}]
+          else
+            val
+          end
+        end)
+      end)
+
+    # figure out which cells flashed this round by comparing w old all_flashed
+    new_all_flashed = new_grid |> get_cells_above(9)
+    new_flashed = new_all_flashed |> MapSet.difference(all_flashed)
+
+    # recurse
+    cascade(new_grid, new_all_flashed, new_flashed, MapSet.size(new_flashed))
+  end
+
+  @spec increment_cells(grid(), coordset()) :: grid()
   def increment_cells(grid, which) do
     grid
-    |> Enum.with_index
+    |> Enum.with_index()
     |> Enum.map(fn {row, y} ->
       row
-      |> Enum.with_index
+      |> Enum.with_index()
       |> Enum.map(fn {val, x} ->
         if {x, y} in which do
           val + 1
@@ -22,11 +76,44 @@ defmodule Aoc21.Day11 do
     end)
   end
 
-  @spec increment_cells([[integer()]]) :: list
+  @spec increment_cells(grid()) :: grid()
   def increment_cells(grid) do
     grid
     |> Enum.map(fn row ->
       row |> Enum.map(fn val -> val + 1 end)
+    end)
+  end
+
+  @spec get_neighbs(coord()) :: coordset()
+  def get_neighbs({x, y}) do
+    [
+      {x - 1, y - 1},
+      {x, y - 1},
+      {x + 1, y - 1},
+      {x + 1, y},
+      {x + 1, y + 1},
+      {x, y + 1},
+      {x - 1, y + 1},
+      {x - 1, y}
+    ]
+    |> Enum.filter(fn {x, y} -> 0 <= x and x < 10 and 0 <= y and y < 10 end)
+    |> MapSet.new()
+  end
+
+  @spec get_cells_above(grid(), integer()) :: coordset()
+  def get_cells_above(grid, thresh) do
+    grid
+    |> Enum.with_index()
+    |> Enum.reduce(MapSet.new(), fn {row, y}, set ->
+      row
+      |> Enum.with_index()
+      |> Enum.reduce(set, fn {val, x}, set ->
+        if val > thresh do
+          MapSet.put(set, {x, y})
+        else
+          set
+        end
+      end)
     end)
   end
 end
--- test/day11_test.exs
+++ test/day11_test.exs
@@ -1,43 +1,61 @@
 defmodule Day11Test do
   use ExUnit.Case
   import Aoc21.Input, only: [readlines: 2]
-  import Aoc21.Day11, only: [increment_cells: 2, increment_cells: 1]
+
+  import Aoc21.Day11,
+    only: [increment_cells: 2, increment_cells: 1, get_neighbs: 1, get_cells_above: 2, step: 2]
 
   setup_all do
-    [grid: readlines("t9.txt", as: [:integer])]
+    [grid: readlines("t11.txt", as: [:integer])]
   end
 
   test "increments all cells" do
     assert [
-      [1, 1, 1, 1, 1],
-      [1, 8, 8, 8, 1],
-      [1, 8, 1, 8, 1],
-      [1, 8, 8, 8, 1],
-      [1, 1, 1, 1, 1]
-    ]
-    |> increment_cells() == [
-      [2, 2, 2, 2, 2],
-      [2, 9, 9, 9, 2],
-      [2, 9, 2, 9, 2],
-      [2, 9, 9, 9, 2],
-      [2, 2, 2, 2, 2]
-    ]
+             [1, 1, 1, 1, 1],
+             [1, 8, 8, 8, 1],
+             [1, 8, 1, 8, 1],
+             [1, 8, 8, 8, 1],
+             [1, 1, 1, 1, 1]
+           ]
+           |> increment_cells() == [
+             [2, 2, 2, 2, 2],
+             [2, 9, 9, 9, 2],
+             [2, 9, 2, 9, 2],
+             [2, 9, 9, 9, 2],
+             [2, 2, 2, 2, 2]
+           ]
   end
 
-  test "increments some cells" do
+  test "increments neighbors of {0,0}" do
     assert [
-      [1, 1, 1, 1, 1],
-      [1, 8, 8, 8, 1],
-      [1, 8, 1, 8, 1],
-      [1, 8, 8, 8, 1],
-      [1, 1, 1, 1, 1]
-    ]
-    |> increment_cells(MapSet.new([{0,0}, {0,1}, {1,0}, {1,1}])) == [
-      [2, 2, 1, 1, 1],
-      [2, 9, 8, 8, 1],
-      [1, 8, 1, 8, 1],
-      [1, 8, 8, 8, 1],
-      [1, 1, 1, 1, 1]
-    ]
+             [1, 1, 1, 1, 1],
+             [1, 8, 8, 8, 1],
+             [1, 8, 1, 8, 1],
+             [1, 8, 8, 8, 1],
+             [1, 1, 1, 1, 1]
+           ]
+           |> increment_cells(get_neighbs({0, 0})) == [
+             [1, 2, 1, 1, 1],
+             [2, 9, 8, 8, 1],
+             [1, 8, 1, 8, 1],
+             [1, 8, 8, 8, 1],
+             [1, 1, 1, 1, 1]
+           ]
+  end
+
+  test "finds squares above 10" do
+    assert [
+             [1, 1, 1, 1, 1],
+             [1, 8, 8, 8, 1],
+             [1, 8, 11, 8, 1],
+             [1, 8, 8, 8, 1],
+             [1, 1, 1, 1, 1]
+           ]
+           |> get_cells_above(10) == MapSet.new([{2, 2}])
+  end
+
+  test "finds correct number of flashes after 10 steps", fixture do
+    {_, flash_cnt} = fixture.grid |> step(10)
+    assert flash_cnt == 204
   end
 end
