ba61e7930900b295dfce817ba662742c9c7ffe33
Andreas Aaberge Eide <ae@pilco.no>
2022-12-10 09:29:45 +0100

    Day 09

--- lib/Aoc22/day09.ex
+++ lib/Aoc22/day09.ex
@@ -1,5 +1,4 @@
 defmodule Aoc22.Day09 do
-  # Not touching and different row and column, diagonal move
   def tail_move({xh, yh}, {xt, yt}) do
     cond do
       # Touching, tail is still
@@ -20,34 +19,44 @@ defmodule Aoc22.Day09 do
 
       abs(xh - xt) == 1 and abs(yh - yt) == 2 ->
         {xh, div(yh + yt, 2)}
+
+      abs(xh - xt) == 2 and abs(yh - yt) == 2 ->
+        {div(xh + xt, 2), div(yh + yt, 2)}
     end
   end
 
-  def head_move("R", log = [{{xh, yh}, xyt} | _]),
-    do: [{{xh + 1, yh}, tail_move({xh + 1, yh}, xyt)} | log]
+  def propagate(_, []), do: []
 
-  def head_move("U", log = [{{xh, yh}, xyt} | _]),
-    do: [{{xh, yh + 1}, tail_move({xh, yh + 1}, xyt)} | log]
+  def propagate(xyh, [xyt | tail]) do
+    next = tail_move(xyh, xyt)
+    [next | propagate(next, tail)]
+  end
 
-  def head_move("L", log = [{{xh, yh}, xyt} | _]),
-    do: [{{xh - 1, yh}, tail_move({xh - 1, yh}, xyt)} | log]
+  def head_move("R", log = [[{xh, yh} | tail] | _]),
+    do: [[{xh + 1, yh} | propagate({xh + 1, yh}, tail)] | log]
 
-  def head_move("D", log = [{{xh, yh}, xyt} | _]),
-    do: [{{xh, yh - 1}, tail_move({xh, yh - 1}, xyt)} | log]
+  def head_move("U", log = [[{xh, yh} | tail] | _]),
+    do: [[{xh, yh + 1} | propagate({xh, yh + 1}, tail)] | log]
 
-  def part1() do
-    File.read!("inputs/22/t9.txt")
+  def head_move("L", log = [[{xh, yh} | tail] | _]),
+    do: [[{xh - 1, yh} | propagate({xh - 1, yh}, tail)] | log]
+
+  def head_move("D", log = [[{xh, yh} | tail] | _]),
+    do: [[{xh, yh - 1} | propagate({xh, yh - 1}, tail)] | log]
+
+  def solve(rope_length) do
+    File.read!("inputs/22/i9.txt")
     |> String.trim()
     |> String.split("\n")
-    |> Enum.map(&Regex.scan(~r/^\w|\d$/, &1))
-    |> Enum.map(fn [[dir], [amt]] -> List.duplicate(dir, String.to_integer(amt)) end)
+    |> Enum.map(&String.split/1)
+    |> Enum.map(fn [dir, amt] -> List.duplicate(dir, String.to_integer(amt)) end)
     |> List.flatten()
-    |> Enum.reduce([{{0, 0}, {0, 0}}], &head_move/2)
-    |> IO.inspect()
-
-    # |> Enum.map(fn {_xyh, xyt} -> xyt end)
-    # |> Enum.uniq()
-    # |> length()
-    # |> IO.puts()
+    |> Enum.reduce([for(_ <- 1..rope_length, do: {0, 0})], &head_move/2)
+    |> Enum.map(&Enum.at(&1, -1))
+    |> Enum.uniq()
+    |> length()
   end
+
+  def part1(), do: solve(2) |> IO.puts()
+  def part2(), do: solve(10) |> IO.puts()
 end
